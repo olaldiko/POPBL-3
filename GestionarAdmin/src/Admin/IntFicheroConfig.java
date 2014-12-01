71aa69151e68a8f5296d332a201bd6e2ff29f4cf
toyovid <david.acedo@alumni.mondragon.edu>
2014-12-01 13:59:14 +0100

    Cambios Interface

--- GestionarAdmin/src/Admin/IntFicheroConfig.java
+++ GestionarAdmin/src/Admin/IntFicheroConfig.java
@@ -2,5 +2,5 @@ package Admin;
 
 public interface IntFicheroConfig {
 	void cargarDatos();
-	void guardarDatos();
+	void guardarDatos(int tamx, int tamy, String URL, int nPort, String nBD, String user, String pass);
 }
